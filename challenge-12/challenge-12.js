97571575f6c9f02dd0b1057927fe59575248d5e9
Fernando Feliu <fernando.goncalez@autotrac.com.br>
2019-01-16 15:58:32 -0200

    Solução do challenge-12

--- challenge-12/challenge-12.js
+++ challenge-12/challenge-12.js
@@ -52,42 +52,43 @@
 
   book3.name = 'Livro 3';
   book3.pages = 50;
-  books.push(book3);  
-  
+  books.push(book3);
+
   console.log( '\nLista de livros:' );
   /*
   Mostre no console todos os livros.
   */
-  for(var i = 0; i < books.length; i++) {
-    console.log(books[i].name);
-  }
+  console.log(books);
+
 
   console.log( '\nLivro que está sendo removido:' );
   /*
   Remova o último livro, e mostre-o no console.
   */
+  books.pop();
 
   console.log( '\nAgora sobraram somente os livros:' );
   /*
   Mostre no console os livros restantes.
   */
-  // ?
+  console.log(books);
+
 
   /*
   Converta os objetos que ficaram em `books` para strings.
   */
-  // ?
+  var booksStr = JSON.stringify(books);
   console.log( '\nLivros em formato string:' );
 
   /*
   Mostre os livros nesse formato no console:
   */
-  // ?
+  console.log(booksStr);
 
   /*
   Converta os livros novamente para objeto.
   */
-  // ?
+  books = JSON.parse(booksStr);
   console.log( '\nAgora os livros são objetos novamente:' );
 
   /*
@@ -95,30 +96,36 @@
   no formato abaixo:
       "[PROPRIEDADE]: [VALOR]"
   */
-  // ?
+ for(var i = 0; i < books.length; i++) {
+
+  for( var prop in books[i]) {
+    console.log(prop + ':' + books[i][prop]);
+  }
+}
+
 
   /*
   Crie um array chamado `myName`. Cada item desse array deve ser uma letra do
   seu nome. Adicione seu nome completo no array.
   */
-  // ?
+  var myName = ['F', 'e', 'r', 'n', 'a', 'n', 'd', 'o'];
   console.log( '\nMeu nome é:' );
 
   /*
   Juntando todos os itens do array, mostre no console seu nome.
   */
-  // ?
-
+  console.log(myName.join(''));
   console.log( '\nMeu nome invertido é:' );
 
   /*
   Ainda usando o objeto acima, mostre no console seu nome invertido.
   */
-  // ?
+  console.log(myName.reverse());
 
   console.log( '\nAgora em ordem alfabética:' );
+
   /*
   Mostre todos os itens do array acima, odenados alfabéticamente.
   */
-  // ?
+  console.log(myName.sort());
 })();
